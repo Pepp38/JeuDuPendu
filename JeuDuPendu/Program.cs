ec34bb29dc2946c7f93e5d055e3043bccc809704
Pepp38 <102177070+Pepp38@users.noreply.github.com>
2025-01-03 19:09:33 +0000

    Création de l'application console et de la fonction Afficher

---
+++ JeuDuPendu/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace JeuDuPendu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mot = "Arachide";
+            
+            AfficherMot(mot);
+            //DevinerMot();
+
+        }
+
+        static void AfficherMot(string mot)
+        {         
+            string motAffiché = ""; 
+
+            for(int i=1;i<=mot.Length;i++)
+            {
+                motAffiché = motAffiché += "_ ";
+            }
+            Console.WriteLine(motAffiché);
+        }
+    }
+}
